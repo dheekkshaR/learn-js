6fe9040400c9935f8ca64b16d9ad13ab7b118683
dheekkshaR <rajeshbabu.d@northeastern.edu>
2024-01-30 16:20:36 -0500

    Commit 2 quizes, logger and arrow

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,21 @@
 // Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+function logMsg(){
+
+    if (this.errMsg){
+        console.log(`Error message  : ${this.errMsg}`);
+    }
+    else{
+        console.log("No error")
+    }
+};
+
+const obj1={
+    errMsg:"Obj 1 ERROR QUIZ FSE"
+}
+const obj2={
+    propertyGiven:"HI"
+}
+
+logMsg.call(obj1);
+logMsg.call(obj2);
+//displays error with name then no error
