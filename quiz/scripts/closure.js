b37582309e39a653428141f1f6ffc83843dc038a
dheekkshaR <rajeshbabu.d@northeastern.edu>
2024-01-30 17:03:38 -0500

    Commit 2 quizes, closure and add event

--- quiz/scripts/closure.js
+++ quiz/scripts/closure.js
@@ -1,9 +1,12 @@
 function setColor(set) {
         let changeColor = set;
-        if(changeColor) {
+        return () => {if(changeColor) {
             let userColor = document.getElementById('color').value;
+            console.log(userColor)
+            console.log(document.getElementById('myPara').style.color)
             document.getElementById('myPara').style.color = userColor;
         }
+    }//body of fucn is returned where change color is bound to setcolor
         
 }
 
